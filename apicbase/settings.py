f4ec22c0161f7b6a37e83e8fb24b4e29cb5661d4
zolicoeur <ci_attatra@esi.dz>
2019-09-16 16:00:52 +0200

    add requirements heroku

--- apicbase/settings.py
+++ apicbase/settings.py
@@ -123,3 +123,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+
+#for heroku
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
